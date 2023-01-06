aadf35c752f6f94de2a2861e66874d1f56f0e49f
AAKEN Developer <aakendev@gmail.com>
2023-01-06 01:58:52 +0000

    Initializing Repo

---
+++ src/routes.js
@@ -0,0 +1,39 @@
+const {
+  testHapiHandler, addBookHandler,
+  getAllBooksHandler, getBookByIdHandler,
+  editBookByIdHandler, deleteBookByIdHandler,
+} = require('./handler');
+
+const routes = [
+  {
+    method: 'GET',
+    path: '/',
+    handler: testHapiHandler,
+  },
+  {
+    method: 'GET',
+    path: '/books',
+    handler: getAllBooksHandler,
+  },
+  {
+    method: 'POST',
+    path: '/books',
+    handler: addBookHandler,
+  },
+  {
+    method: 'GET',
+    path: '/books/{bookId}',
+    handler: getBookByIdHandler,
+  },
+  {
+    method: 'PUT',
+    path: '/books/{bookId}',
+    handler: editBookByIdHandler,
+  },
+  {
+    method: 'DELETE',
+    path: '/books/{bookId}',
+    handler: deleteBookByIdHandler,
+  },
+];
+module.exports = routes;
